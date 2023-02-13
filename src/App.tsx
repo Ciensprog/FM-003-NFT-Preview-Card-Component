687155ac7204bcdcc8f5e85ba3be9bd295cfbb61
Ciensprog <jorg.leonrd@gmail.com>
2023-02-12 23:36:54 -0600

    Add new colors

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,9 @@ export function App() {
   return (
     <>
       <main className="my-10 z-10">
-        <h1 className="font-medium text-2xl">NFT Preview Card Component</h1>
+        <h1 className="font-medium text-2xl text-soft-blue">
+          NFT Preview Card Component
+        </h1>
       </main>
 
       <Attribution />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
 // const { fontFamily } = require('tailwindcss/defaultTheme')
 
 const colors = {
-  //
+  cyan: '#00FFF8',
+  'dark-blue': {
+    400: '#2E405A',
+    700: '#15263F',
+    900: '#0D192C',
+  },
+  'soft-blue': '#8BACD9',
 }
 
 /** @type {import('tailwindcss').Config} */
